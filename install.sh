3ddb92f5bc0ec6abece77aa5f48155f45439a952
Kitt K <kittipitch@gmail.com>
2016-03-11 00:12:23 +0700

    gfill installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+# @Author: Kitt K
+# @Date:   2016-03-11 00:09:25
+# @Last Modified by:   Kitt K
+# @Last Modified time: 2016-03-11 00:11:27
+
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo pip3 install virtualenv
+virtualenv ./scripts/gfill
+./scripts/bin/pip3 install -r REQUIREMENTS.txt
