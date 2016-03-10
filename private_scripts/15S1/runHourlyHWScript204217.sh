eec92b43f619320d4aa1686c1a4012c4c7552cb4
Kitt K <kittipitch@gmail.com>
2016-03-11 03:14:17 +0700

    install

--- private_scripts/15S1/runHourlyHWScript204217.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-class="204217"
-hourlyScript="TRUE"
-
-. /home/kittipitch/private_scripts/runDailyHWScript"$class".sh
-
-
--- private_scripts/graderRankUpdate/rankingGsheet/sshToGrader.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/screen -S manualGraderConnect -d -m /bin/bash -c '/usr/bin/ssh -L 8889:127.0.0.1:8889 grader@10.10.10.175'
