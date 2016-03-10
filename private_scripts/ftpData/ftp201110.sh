0b4cb607d3b81f4110f28ba14bf567141d16fdca
Kitt K <kittipitch@gmail.com>
2016-03-10 22:25:51 +0700

    removed credential

--- private_scripts/ftpData/ftp201110.sh
+++
@@ -1,5 +0,0 @@
-#ftpHost='hw.cs.science.cmu.ac.th'
-ftpHost='202.28.248.59'
-ftpUser='hw201110'
-ftpPasswd='hw1011@c$'
-
--- private_scripts/ftpData/ftp204202.sh
+++
@@ -1,5 +0,0 @@
-#ftpHost='hw.cs.science.cmu.ac.th'
-ftpHost='202.28.248.59'
-ftpUser='hw204202'
-ftpPasswd='hw0242@c$'
-
